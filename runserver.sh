3fae2531532c381ecfe2f403e27acee97b5bc6a1
Huan Liu <louieliu.us@gmail.com>
2017-11-16 15:50:47 -0800

    new

---
+++ runserver.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Dummy jar file
+java -jar Server.jar
+
+#TCP Server
+#java -jar Server.jar ts 1234
+
+#UDP Server
+#java -jar Server.jar us 1234
+
+#RMI Server
+#rmiregistry -J-Djava.class.path= Server.jar &
+#java -Djava.rmi.server.codebase= Server.jar -cp Server.jar Server.Server rmi
+
